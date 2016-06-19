47e6667dd6d1cbb080a7d1a8ea3d7a32479d3c19
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-06-19 12:13:57 -0300

    Creating get and post method for login view

--- inspecWeb/authentication/views.py
+++ inspecWeb/authentication/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+"""Views for authentication app."""
 
-# Create your views here.
+from django.shortcuts import render, render_to_response, redirect
+from django.template import RequestContext
+from django.views.generic import View
+from django.contrib.auth import authenticate, login
+
+
+class Login(View):
+    """Login methods for auth."""
+
+    http_method_names = [u'get', u'post']
+
+    def get(self, request):
+        """Get method for login."""
+        if request.user.is_authenticated():
+            response = redirect('/')
+        else:
+            response = render_to_response(
+                'login.html', context_instance=RequestContext(request))
+        return response
+
+    def post(self, request):
+        """POST method for login."""
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username, password)
+
+        if user is not None:
+            login(request, user)
+            response = render(request, 'index.html', user)
+        else:
+            response = redirect('/login/')
+        return response
