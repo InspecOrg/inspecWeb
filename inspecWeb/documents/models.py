d90e769d13e7bfeeba7e836552e6d61cde2bc55e
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-06-20 17:52:09 -0300

    adding Document classes

--- inspecWeb/documents/models.py
+++ inspecWeb/documents/models.py
@@ -1,5 +1,32 @@
-from __future__ import unicode_literals
+"""Document models."""
 
 from django.db import models
+from authentication.models import InspecUser, InspecAgent
 
-# Create your models here.
+
+class Document(models.Model):
+    """Abstract document model."""
+
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+
+
+class Undersigned(Document):
+    """Concrete class inherits from document."""
+
+    signers = models.ForeignKey(InspecUser)
+    status = models.CharField(max_length=100)
+
+    def get_signers():
+        """Get the signers to inform the status of this document."""
+        return None
+
+    def notify_observer():
+        """Notify all observers."""
+        return None
+
+
+class Report(Document):
+    """Concrete class inherits from document."""
+
+    responsible = models.ForeignKey(InspecAgent)
