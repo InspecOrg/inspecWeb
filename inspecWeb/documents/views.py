f5d01e24b3771a622e6f9863e444c067005d3135
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-06-20 18:41:50 -0300

    creating module for documents urls

--- inspecWeb/documents/views.py
+++ inspecWeb/documents/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+"""Views for document app."""
 
+from django.shortcuts import render, render_to_response
+from django.views.generic import View
+from django.template import RequestContext
 # Create your views here.
+
+
+class CreateDocument(View):
+    """Create Document method for both Undersigned and Report."""
+
+    def get(self, request):
+        """Get method for CreateDocument."""
+        return render_to_response(
+            'index.html', context_instance=RequestContext(request))
