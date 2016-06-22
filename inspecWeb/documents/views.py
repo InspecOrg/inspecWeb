4a4ecaeb956ca633b2f0b9858b3933bf8817a37a
Gustavo Sabino <gustavo.sabino@hotmail.com>
2016-06-22 17:42:00 -0300

    creating classes for search

--- inspecWeb/documents/views.py
+++ inspecWeb/documents/views.py
@@ -13,3 +13,16 @@ class CreateDocument(View):
         """Get method for CreateDocument."""
         return render_to_response(
             'index.html', context_instance=RequestContext(request))
+
+    def post(self, request):
+        """Post method for CreateDocument."""
+        return None
+
+
+class ShowDocuments(View):
+    """Class to show the releted documents."""
+
+    def get(self, request):
+        """Get method for acess Related Documents page."""
+        return render_to_response(
+            'documents.html', context_instance=RequestContext(request))
